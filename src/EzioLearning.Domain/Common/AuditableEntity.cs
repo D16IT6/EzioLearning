3ed0a794647f93a25d6df6545731f43c6a10b71f
TalonEzio <vuthemanh1707@gmail.com>
2024-01-17 23:41:17 +0700

    Add project files.

---
+++ src/EzioLearning.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EzioLearning.Domain.Common;
+
+internal class AuditableEntity<T>
+{
+    public DateTime CreatedDate { get; set; }
+    public T? CreatedBy { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public T? ModifiedBy { get; set; }
+}
+
